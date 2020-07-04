5a24d3e00649b3e07c8ab24297e60323cd4def46
gugucon-hwan <gugucon2018@gmail.com>
2020-07-05 06:57:15 +0900

    [4Chpater] 푸는 중 현재 8번까지 품

---
+++ practice4_Q/src/practice4_Q/Practice4_7.java
@@ -0,0 +1,11 @@
+package practice4_Q;
+
+/*Chapter 4-7 자바의 정석 개인 정답 */
+public class Practice4_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = (int)(Math.random()*6)+1;
+		System.out.println("value:"+value);
+	}
+}
---
+++ practice4_Q/src/practice4_Q/Practice4_8.java
@@ -0,0 +1,15 @@
+package practice4_Q;
+/*Chapter 4-8 자바의 정석 개인 정답 */
+public class Practice4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		for(int x=0;x<=10;x++) {
+			for(int y=0;y<=10;y++)
+			if(2*x+4*y==10) {
+				System.out.println("x="+x+" , y="+y);
+			}
+		}		
+	}
+}
