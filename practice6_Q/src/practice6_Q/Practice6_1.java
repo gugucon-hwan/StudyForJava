7c324dfb5cc2ef285e69faee65c14dd6d3bd35f3
gugucon-hwan <gugucon2018@gmail.com>
2020-08-12 23:39:03 +0900

    문제 chapter 6-1 완료

---
+++ practice6_Q/src/practice6_Q/Practice6_1.java
@@ -0,0 +1,22 @@
+package practice6_Q;
+
+public class Practice6_1 {
+/*
+ * [6-1] 다음과 같은 멤버변수를 갖는 SutdaCard클래스를 정의하시오.
+ *     타입        변수명            설 명
+	    int           num        카드의 숫자.(1~10사이의 정수)
+	 boolean    isKwang     광(光)이면 true, 아니면 false
+ * 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		
+	}	
+
+}
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+}
