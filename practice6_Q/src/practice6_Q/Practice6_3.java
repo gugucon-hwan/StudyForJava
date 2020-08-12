3dc7af5b832b31e159668a87d08b875e92c82dda
gugucon-hwan <gugucon2018@gmail.com>
2020-08-13 00:20:40 +0900

    문제 6-3

---
+++ practice6_Q/src/practice6_Q/Practice6_3.java
@@ -0,0 +1,32 @@
+package practice6_Q;
+
+/*
+ * [6-3] 다음과 같은 멤버변수를 갖는 Student클래스를 정의하시오.
+ * 타 입 변수명 설 명
+ * String name 학생이름
+ *   int    ban     반
+ *   int     no    번호
+ *   int    kor    국어점수
+ *   int    eng   영어점수
+ *   int   math  수학점수
+ */
+
+public class Practice6_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+class Student{
+	String name;//학생이름
+	int ban;		//반
+	int no;		//번호
+	int kor;		//국어점수
+	int eng;		//영어점수
+	int math;	//수학점수
+	
+	
+}
